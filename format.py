1bc2997e1d6ba5b162e509a29f4fefe8ace98b7a
Alexey Antipin <scottyfionnghall@gmail.com>
2022-05-28 19:45:12 +0300

    first commit

---
+++ format.py
@@ -0,0 +1,21 @@
+import re
+import markovify
+
+with open("placeyourtxthere.txt", encoding="utf8") as f:
+    text = f.read()
+    f.close()
+lines = text.split("\n")
+non_empty_lines = [line for line in lines if line.strip() != ""]
+string_without_empty_lines = ""
+for line in non_empty_lines:
+    x = re.findall('\[|http|:|{|m!|None|-', line)
+    if not x:
+        string_without_empty_lines += line + "\n"
+with open('placeyourtxthere_formatted.txt', 'w', encoding='utf-8') as f:
+      f.write(string_without_empty_lines)
+      f.close
+with open('corpus.json','w',encoding='utf-8') as f:
+    text_model = markovify.NewlineText(string_without_empty_lines, state_size=2)
+    model_json = text_model.to_json()
+    f.write(model_json)
+    f.close
---
+++ makememe.py
@@ -0,0 +1,30 @@
+import markovify
+import re
+import spacy
+
+nlp = spacy.load("ru_core_news_sm")
+
+class POSifiedText(markovify.NewlineText):
+    def word_split(self, sentence):
+        return ["::".join((word.orth_, word.pos_)) for word in nlp(sentence)]
+
+    def word_join(self, words):
+        sentence = " ".join(word.split("::")[0] for word in words)
+        return sentence
+
+with open("corpus.json") as f:
+    text = f.read()
+    f.close()
+with open("corpus2.json") as f:
+    text2 = f.read()
+    f.close()
+
+# Build the model.
+text_model1 = markovify.NewlineText.from_json(text)
+text_model2 = markovify.NewlineText.from_json(text2)
+model_cobmo = markovify.combine([ text_model1,text_model2 ], [ 2,1 ])
+# Print five randomly-generated sentences
+for i in range(120):
+    with open("output2.txt",'a',encoding='utf-8') as f:
+        f.write(str(model_cobmo.make_sentence())+'\n')
+        f.close
