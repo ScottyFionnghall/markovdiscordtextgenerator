446bb41f4338d84d4620b5a41152721aec4dd631
Alexey Antipin <scottyfionnghall@gmail.com>
2022-05-28 23:07:00 +0300

    first commit

--- format.py
+++ format.py
@@ -1,7 +1,7 @@
 import re
 import markovify
 
-with open("placeyourtxthere.txt", encoding="utf8") as f:
+with open("filename.txt", encoding="utf8") as f:
     text = f.read()
     f.close()
 lines = text.split("\n")
@@ -11,11 +11,11 @@ for line in non_empty_lines:
     x = re.findall('\[|http|:|{|m!|None|-', line)
     if not x:
         string_without_empty_lines += line + "\n"
-with open('placeyourtxthere_formatted.txt', 'w', encoding='utf-8') as f:
+with open('filename_formatted.txt', 'w', encoding='utf-8') as f:
       f.write(string_without_empty_lines)
       f.close
 with open('corpus.json','w',encoding='utf-8') as f:
-    text_model = markovify.NewlineText(string_without_empty_lines, state_size=2)
+    text_model = markovify.Text(text, state_size=2)
     model_json = text_model.to_json()
     f.write(model_json)
     f.close
--- makememe.py
+++ makememe.py
@@ -2,8 +2,9 @@ import markovify
 import re
 import spacy
 
-nlp = spacy.load("ru_core_news_sm")
+nlp = spacy.load("en_core_web_sm")
 
+# Use space ... don't how it works and if it works at all
 class POSifiedText(markovify.NewlineText):
     def word_split(self, sentence):
         return ["::".join((word.orth_, word.pos_)) for word in nlp(sentence)]
@@ -11,7 +12,7 @@ class POSifiedText(markovify.NewlineText):
     def word_join(self, words):
         sentence = " ".join(word.split("::")[0] for word in words)
         return sentence
-
+# Open files
 with open("corpus.json") as f:
     text = f.read()
     f.close()
@@ -23,7 +24,7 @@ with open("corpus2.json") as f:
 text_model1 = markovify.NewlineText.from_json(text)
 text_model2 = markovify.NewlineText.from_json(text2)
 model_cobmo = markovify.combine([ text_model1,text_model2 ], [ 2,1 ])
-# Print five randomly-generated sentences
+# Print 120 randomly-generated sentences
 for i in range(120):
     with open("output2.txt",'a',encoding='utf-8') as f:
         f.write(str(model_cobmo.make_sentence())+'\n')
